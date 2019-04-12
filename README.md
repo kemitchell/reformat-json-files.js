abfa9e79f8fbe576fc218ef626b005a76d4e64d9
Kyle E. Mitchell <kyle@kemitchell.com>
2019-04-12 12:06:54 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+reformat JSON files in-place with `JSON.stringify(data, null, 2)`
+
+```shellsession
+npx reformat-json-files package.json other.json
+```
---
+++ bin.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+if (process.argv.length < 3) {
+  process.stderr.write('Usage: format-json-files <file> (<file>...)\n')
+  process.exit(1)
+}
+
+var fs = require('fs')
+
+process.argv.slice(2).forEach(function (file) {
+  try {
+    var json = fs.readFileSync(file)
+  } catch (error) {
+    process.stderr.write('Could not read ' + file + '\n')
+    process.exit(1)
+  }
+
+  try {
+    var parsed = JSON.parse(json)
+  } catch (error) {
+    process.stderr.write('Invalid JSON:' + file + '\n')
+    process.exit(1)
+  }
+
+  fs.writeFileSync(file, JSON.stringify(parsed, null, 2))
+})
